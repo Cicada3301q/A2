0bb33f92eead8cd5692e2155a2e1ca0d5a166acc
Quenten <quentenwelch@gmail.com>
2023-10-15 17:59:12 -0600

    Handle arrays in field inspector

--- ObjectInspector.java
+++ ObjectInspector.java
@@ -20,7 +20,10 @@ import java.util.*;
 import java.lang.reflect.*;
 
 public class ObjectInspector {
+	private Set<Class<?>> inspectedClasses;
+
 	public ObjectInspector() {
+		inspectedClasses = new HashSet<>();
 	}
 
 	// -----------------------------------------------------------
@@ -31,11 +34,13 @@ public class ObjectInspector {
 		System.out.println("inside inspector: " + obj + " (recursive = " + recursive + ")");
 
 		// inspect the current class
-		inspectFields(obj, ObjClass, objectsToInspect);
+		inspectFields(obj, ObjClass, objectsToInspect, inspectedClasses);
 
 		if (recursive)
 			inspectFieldClasses(obj, ObjClass, objectsToInspect, recursive);
 
+		// Call the new method to inspect constructors
+		inspectConstructors(ObjClass, inspectedClasses);
 	}
 
 	// -----------------------------------------------------------
@@ -61,36 +66,84 @@ public class ObjectInspector {
 	}
 
 	// -----------------------------------------------------------
-	private void inspectFields(Object obj, Class ObjClass, Vector objectsToInspect)
+	private void inspectFields(Object obj, Class<?> objClass, Vector<Object> objectsToInspect,
+			Set<Class<?>> inspectedClasses) {
+		Class<?> currentClass = objClass;
 
-	{
-
-		Field[] fields = ObjClass.getDeclaredFields();
-
-		for (Field field : fields) {
-			field.setAccessible(true);
-			String fieldName = field.getName();
-			Class<?> fieldType = field.getType();
-			int modifiers = field.getModifiers();
-
-			System.out.println("Field Name: " + fieldName);
-			System.out.println("Field Type: " + fieldType.getName());
-			System.out.println("Modifiers: " + Modifier.toString(modifiers));
-
-			try {
-				Object fieldValue = field.get(obj);
-				System.out.println("Field Value: " + fieldValue);
-
-				if (fieldValue != null && !fieldType.isPrimitive()) {
-					objectsToInspect.addElement(fieldValue);
-				}
-			} catch (Exception e) {
-				// Handle exceptions appropriately, e.g., log or throw
+		while (currentClass != null) {
+			if (inspectedClasses.contains(currentClass)) {
+				break; // Avoid re-inspecting the same class
 			}
-		}
 
-		if (ObjClass.getSuperclass() != null) {
-			inspectFields(obj, ObjClass.getSuperclass(), objectsToInspect);
+			Field[] fields = currentClass.getDeclaredFields();
+
+			for (Field field : fields) {
+				field.setAccessible(true);
+				String fieldName = field.getName();
+				Class<?> fieldType = field.getType();
+				int modifiers = field.getModifiers();
+
+				System.out.println("Field Name: " + fieldName);
+				if (fieldType.isArray()) {
+					System.out.println("Field Type: " + fieldType.getComponentType().getName());
+				} else {
+					System.out.println("Field Type: " + fieldType.getName());
+				}
+				System.out.println("Modifiers: " + Modifier.toString(modifiers));
+
+				try {
+					Object fieldValue = field.get(obj);
+					if (fieldValue != null && fieldType.isArray()) {
+						int length = Array.getLength(fieldValue);
+						System.out.println("Array Length: " + length);
+						for (int i = 0; i < length; i++) {
+							Object arrayElement = Array.get(fieldValue, i);
+							System.out.println("Element " + i + ": " + arrayElement);
+						}
+					} else {
+						System.out.println("Field Value: " + fieldValue);
+					}
+
+					if (fieldValue != null && !fieldType.isPrimitive()) {
+						objectsToInspect.addElement(fieldValue);
+					}
+				} catch (Exception e) {
+					// Handle exceptions appropriately, e.g., log or throw
+				}
+			}
+
+			inspectedClasses.add(currentClass);
+			currentClass = currentClass.getSuperclass();
+		}
+	}
+
+	private void inspectConstructors(Class<?> objClass, Set<Class<?>> inspectedClasses) {
+		Class<?> currentClass = objClass;
+
+		while (currentClass != null) {
+			// if (inspectedClasses.contains(currentClass)) {
+			// break; // Avoid re-inspecting the same class
+			// }
+
+			Constructor<?>[] constructors = currentClass.getDeclaredConstructors();
+
+			for (Constructor<?> constructor : constructors) {
+				String constructorName = constructor.getName();
+				int modifiers = constructor.getModifiers();
+				Class<?>[] parameterTypes = constructor.getParameterTypes();
+
+				System.out.println("Constructor Name: " + constructorName);
+				System.out.println("Modifiers: " + Modifier.toString(modifiers));
+
+				System.out.print("Parameter Types: ");
+				for (Class<?> parameterType : parameterTypes) {
+					System.out.print(parameterType.getName() + " ");
+				}
+				System.out.println();
+			}
+
+			// inspectedClasses.add(currentClass);
+			currentClass = currentClass.getSuperclass();
 		}
 	}
 }
